808b7f30d91001bff95a6b09ec8dafcbd052fb73
Ranjithkumar <nseit_rs@vendor.nse.co.in>
2023-10-14 19:03:23 +0530

    MongoDB session store created and session data will get stored while hits the request

--- config.js
+++ config.js
@@ -2,5 +2,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
+    sessionDBURI: process.env.sessionDBURI,
+    port: process.env.port,
     secret: process.env.secret
 };
--- server.js
+++ server.js
@@ -1,18 +1,41 @@
 const express = require('express');
 const session = require('express-session');
-const app = express();
 const { secret } = require("./config");
+const mongoose = require('mongoose');
+const mongoDBStore = require('connect-mongodb-session')(session);
+const app = express();
 
-app.use(session({
-    resave: false,
-    saveUninitialized: false,
-    secret: secret
-}));
+const sessionDBURI = "mongodb://localhost:27017/sessions";
 
-app.get("/", (req,res)=>{
-    req.session.isAuth = true;
-    console.log(`session `,req.sessionID);
-    res.send("Hello There! Welcome..")
-})
+mongoose
+    .connect(sessionDBURI.toString(), {
+        useNewUrlParser: true,
+        // useCreateIndex: true,
+        useUnifiedTopology: true
+    })
+    .then((res)=>{
+        console.log('MongoDB connected.')
+    })
 
-app.listen(5000, console.log("Server running successfully.."));
+const store = new mongoDBStore({
+                    uri: sessionDBURI,
+                    collection: "mySessions"
+                });
+
+app
+    .use(session({
+        resave: false,
+        saveUninitialized: false,
+        secret: secret,
+        store: store
+    }));
+
+app
+    .get("/", (req,res)=>{
+        req.session.isAuth = true;
+        console.log(`session `,req.sessionID);
+        res.send("Hello There! Welcome..")
+    })
+
+app
+    .listen(5000, console.log("Server running successfully.."));
