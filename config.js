923968a9b4d08f111537980ad4299ba4fd57fba7
Ranjithkumar <nseit_rs@vendor.nse.co.in>
2023-10-14 17:48:15 +0530

    session created and session ID sent to client

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+const dotenv = require('dotenv');
+dotenv.config();
+module.exports = {
+    secret: process.env.secret
+};
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const session = require('express-session');
 const app = express();
+const { secret } = require("./config");
+
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: secret
+}));
 
 app.get("/", (req,res)=>{
+    req.session.isAuth = true;
+    console.log(`session `,req.sessionID);
     res.send("Hello There! Welcome..")
 })
 
