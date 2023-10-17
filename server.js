0a9758aa1e315ff1dd20bcf883d4a2e1a9ae7840
Ranjithkumar <nseit_rs@vendor.nse.co.in>
2023-10-17 19:35:38 +0530

    Node file system module CRUD file

--- server.js
+++ server.js
@@ -1,5 +1,40 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const app = express();
+
+const filePath = path.join(`${__dirname}/dataFiles`, "testFile2.txt");
+
+//File creation
+// fs.writeFile(filePath, "This is a simple text file.", (err)=>{
+//     if(!err) console.log('File created Successfully! ');
+//     else console.log('err ', err);
+// });
+
+//File read
+// fs.readFile(filePath, 'utf-8' ,(err, data)=> {
+//     if(!err) console.log(data);
+//     else console.log(err);
+// })
+
+//File update
+// fs.appendFile(filePath, " New contents are updated.", (err) => {
+//     if(!err) console.log('File successfully updated!')
+//     else console.log(err);
+// })
+
+//File rename
+// fs.rename(filePath, `${__dirname}/dataFiles/testFile2.txt`, (err) => {
+//     if(!err) console.log('File rename has done!');
+//     else console.log('err ', err);
+// })
+
+//File delete
+fs.unlink(filePath, (err) => {
+    if(!err) console.log('File deleted!');
+    else console.log(err);
+});
+
 
 app.get("/", (req,res)=>{
     res.send("Hello There! Welcome..")
