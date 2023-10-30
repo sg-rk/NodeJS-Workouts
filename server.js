0a121c1c4d17e6bcba03760fc8b71dcf18b164ed
Ranjithkumar <nseit_rs@vendor.nse.co.in>
2023-10-30 20:16:29 +0530

    code changed to Clean maintainable

--- server.js
+++ server.js
@@ -12,54 +12,61 @@ let dataStore = [
     }
 ]
 
-http.createServer((req, res)=>{
-    const {headers, method} = req;
+http.createServer((req, res) => {
+    const { method } = req;
     const url = urlModule.parse(req.url, true);
     console.log(url);
-    req.on('error', (err)=>{
-        console.error('rk ', err);
+    req.on('error', (err) => {
+        console.error('error ', err);
         res.statusCode = 400;
     });
 
-    res.on('error', (err)=>{
+    res.on('error', (err) => {
         console.error(err);
     })
-    
-    if(method == 'GET'){
-        switch (url.pathname) {
-            case "/echo":
-                echoFunction(req, res);
-                break;
-            case "/projects":
-                fetchProject(req, res, url);
-                break;
-            default:
-                apiNotFound(req, res);
-                break;
-        }
-    }
-    else if(method == "POST"){
-        switch (url.pathname) {
-            case "/projects":           
-                saveProject(req, res);
-                break;        
-            case "/projects/delete":
-                deleteProject(req, res, url);
-                break;
-            case "/projects/update":
-                updateProject(req, res, url);
-                break;
-            default:
-                apiNotFound(req, res);
-                break;
-        }
-    }
-    else if( url.pathname == '/'){
+
+    if (url.pathname == '/') {
         res.statusCode = 200;
-        res.end('Server running!');
+        res.end('Hello world!');
+    }
+    else if (!url || !url.pathname) {
+        apiNotFound(req, res);
     }
     else {
-        apiNotFound(req, res);
+        switch (method) {
+            case "GET":
+                switch (url.pathname) {
+                    case "/echo":
+                        echoFunction(req, res);
+                        break;
+                    case "/projects":
+                        fetchProject(req, res, url);
+                        break;
+                    default:
+                        apiNotFound(req, res);
+                        break;
+                }
+                break;
+            case "POST":
+                switch (url.pathname) {
+                    case "/projects":
+                        saveProject(req, res);
+                        break;
+                    case "/projects/delete":
+                        deleteProject(req, res, url);
+                        break;
+                    case "/projects/update":
+                        updateProject(req, res, url);
+                        break;
+                    default:
+                        apiNotFound(req, res);
+                        break;
+                }
+                break;
+            default:
+                apiNotFound(req, res);
+                break;
+        }
     }
 }).listen(5000);
 
@@ -68,22 +75,32 @@ const apiNotFound = (req, res) => {
     res.end('API not found!')
 }
 
+const recordNotFound = (res) => {
+    res.statusCode = 404;
+    res.write('Record not found!')
+}
+
+const writeContent = (res, statusCode, content) => {
+    res.statusCode = statusCode;
+    res.write(content);
+}
+
 const echoFunction = (req, res) => {
     let body = [];
     req
-    .on('data', (chunk) => {
-        body.push(chunk);
-    })
-    .on('end', ()=>{
-        body = Buffer.concat(body);
-        res
-            .on('error', err => {
-                console.log(err);
-            })
-        res.statusCode = 201;
-        res.write(body);
-        res.end();
-    });
+        .on('data', (chunk) => {
+            body.push(chunk);
+        })
+        .on('end', () => {
+            body = Buffer.concat(body);
+            res
+                .on('error', err => {
+                    console.log(err);
+                })
+            res.statusCode = 201;
+            res.write(body);
+            res.end();
+        });
 }
 
 const saveProject = (req, res) => {
@@ -95,21 +112,19 @@ const saveProject = (req, res) => {
         .on('end', () => {
             body = JSON.parse(Buffer.concat(body).toString());
             let isExists = false;
-            for(let d of dataStore){
-                if(body.id == d.id){
+            for (let d of dataStore) {
+                if (body.id == d.id) {
                     isExists = true;
                     break;
                 }
             }
 
-            if(isExists){
-                res.statusCode = 302;
-                res.write('Record already exists!');
+            if (isExists) {
+                writeContent(res, 302, 'Record already exists!');
             }
             else {
-                dataStore.push(body);                    
-                res.statusCode = 201;
-                res.write('Data saved successfully!')
+                dataStore.push(body);
+                writeContent(res, 201, 'Data saved successfully!');
             }
             console.log(dataStore);
             res.end();
@@ -117,64 +132,58 @@ const saveProject = (req, res) => {
 };
 
 const fetchProject = (req, res, url) => {
-    if(!!url.query && !!url.query.id){
+    if (!!url.query && !!url.query.id) {
         let data = null;
-        for(let d of dataStore){
-            if(url.query.id == d.id){
+        for (let d of dataStore) {
+            if (url.query.id == d.id) {
                 data = JSON.stringify(d);
                 break;
             }
         }
-        if(!!data) {
-            res.write(data);
-            res.statusCode = 200;
+        if (!!data) {
+            writeContent(res, 200, data);
         }
         else {
-            res.statusCode = 404;
-            res.write('Record not found!')
+            recordNotFound(res);
         }
     }
     else {
-        if(url.query.id == '') {
-            res.write('Invalid params!');
-            res.statusCode = 400;    
+        if (url.query.id == '') {
+            writeContent(res, 400, 'Invalid params!');
         }
         else {
-            res.write(JSON.stringify(dataStore));
-            res.statusCode = 200;    
+            writeContent(res, 200, JSON.stringify(dataStore));
         }
     }
     res.end();
 }
 
 const deleteProject = (req, res, url) => {
-    if(url.query && url.query.id){
+    if (url.query && url.query.id) {
         let isDeleted = false;
-        for(let [i, d] of dataStore.entries()){
-            if(url.query.id == d.id){
+        for (let [i, d] of dataStore.entries()) {
+            if (url.query.id == d.id) {
                 dataStore.splice(i, 1);
                 isDeleted = true;
                 break;
             }
         }
-        if(isDeleted) {
-            res.statusCode = 200;
-            res.write('Record delete!')
-        }else {
-            res.statusCode = 404;
-            res.write('Record not found!');    
+
+        if (isDeleted) {
+            writeContent(res, 200, 'Record delete!');
+        } else {
+            recordNotFound(res);
         }
         res.end();
     }
     else {
-        res.statusCode = 404;
-        res.write('Record not found!');
+        recordNotFound(res);
     }
     res.end();
 }
 
 const updateProject = (req, res, url) => {
-    if(url.query && url.query.id){
+    if (url.query && url.query.id) {
         let body = [];
         req
             .on('data', (chunk) => {
@@ -183,27 +192,25 @@ const updateProject = (req, res, url) => {
             .on('end', () => {
                 body = JSON.parse(Buffer.concat(body).toString());
                 let isUpdated = false;
-                for(let [i, d] of dataStore.entries()){
-                    if(url.query.id == d.id){
+                for (let [i, d] of dataStore.entries()) {
+                    if (url.query.id == d.id) {
                         dataStore[i] = body;
                         isUpdated = true;
                         break;
                     }
                 }
-                if(isUpdated) {
-                    res.statusCode = 200;
-                    res.write('Record updated!');
-                    res.write(`\n${JSON.stringify(body)}`);
-                }else {
-                    res.statusCode = 404;
-                    res.write('Record not found!');    
+                if (isUpdated) {
+                    writeContent(res, 200,
+                        `Record updated! \n${JSON.stringify(body)} `);
+
+                } else {
+                    recordNotFound(res);
                 }
                 res.end();
             });
     }
     else {
-        res.statusCode = 404;
-        res.write('Record not found!');
+        recordNotFound(res);
         res.end();
     }
 }
