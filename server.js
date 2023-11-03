bb3bd96001664c005adb3d749ca3fc31548b0f8a
Ranjithkumar <nseit_rs@vendor.nse.co.in>
2023-11-03 20:15:54 +0530

    Web socket setup

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-const express = require('express');
-const app = express();
+const WebSocket = require('ws');
 
-app.get("/", (req,res)=>{
-    res.send("Hello There! Welcome..")
+const wss = new WebSocket.Server({ port: 8082 }, () => {
+    console.log('Web socket server started!')
+});
+
+wss.on('connection', (ws) => {
+    ws.on('open', (d) => {
+        console.log(`Client new connection ${d}`);
+    })
+
+    ws.on('message', (m) => {
+        console.log('Message from client : ', m.toString());
+    })
+
+    ws.send('Hello Client, Welcome to webSocket world!')
 })
-
-app.listen(5000, console.log("Server running successfully.."));
